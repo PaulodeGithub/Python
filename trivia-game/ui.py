232b8b1beb4d264785fc8f663dc0d4106c41fe21
PaulyGallagher <73261024+PaulyGallagher@users.noreply.github.com>
2023-06-19 10:41:12 +0100

    added functionality to buttons

--- trivia-game/ui.py
+++ trivia-game/ui.py
@@ -1,10 +1,12 @@
 from  tkinter import *
+from quiz_brain import QuizBrain
+ 
 
 THEME_COLOR = "#375362"
 
 class QuizInterFace:
 
-        def __init__(self, quiz_brain) :
+        def __init__(self, quiz_brain: QuizBrain) :
              self.quiz = quiz_brain
              self.window = Tk()
              self.title = "Quizzler"
@@ -16,7 +18,8 @@ class QuizInterFace:
              self.canvas = Canvas(width=300, height=250, bg="white")
              self.question_text = self.canvas.create_text(
                     150, 
-                    125, 
+                    125,
+                    width= 280, 
                     text="your question here", 
                     fill=THEME_COLOR,
                     font=("Aerial", 20, "italic"),
@@ -25,17 +28,31 @@ class QuizInterFace:
              self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
              
              true_image = PhotoImage(file="images/true.png")
-             self.true_button = Button(image=true_image, highlightthickness=0)
+             self.true_button = Button(image=true_image, highlightthickness=0, 
+                                       command=self.true_pressed)
              self.true_button.grid(row=2, column=0)
 
              false_image = PhotoImage(file="images/false.png")
-             self.false_button = Button(image=false_image, highlightthickness=0)
+             self.false_button = Button(image=false_image, highlightthickness=0, 
+                                        command=self.false_pressed)
              self.false_button.grid(row=2, column=1)
 
 
-        def get_next_question(self):
+        
+             self.get_next_question()
+
 
              self.window.mainloop()
 
 
+        def get_next_question(self):
+             q_text =self.quiz.next_question()
+             self.canvas.itemconfig(self.question_text, text=q_text)
 
+        def true_pressed(self):
+              self.quiz.check_answer("True")
+              pass
+        
+        
+        def false_pressed(self):
+             self.quiz.check_answer("False")
