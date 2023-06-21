0ef8fae285a3d884da44641c4daf35e5156f5805
PaulyGallagher <73261024+PaulyGallagher@users.noreply.github.com>
2023-06-21 12:07:21 +0100

    parsing json

--- Rainchecker/main.py
+++ Rainchecker/main.py
@@ -2,12 +2,12 @@ import requests
 
 
 OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
-my_key = "<PUT YOUR KEY HERE!>"
+my_key = "INSERT YOUR API KEY HERE!"
 
 
 weather_params = {
-    "lat": -12.998823,
-    "lon" : -38.447058, 
+    "lat": 54.95,
+    "lon" : -7.73333, 
     "appid" : my_key,
     "exclude" : "comment,minutely,daily"
     }
@@ -15,5 +15,17 @@ weather_params = {
 response = requests.get(OWM_Endpoint, weather_params)
 response.raise_for_status()
 weather_data = response.json()
-print(response.status_code)
-print(weather_data)
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = True
+
+for hour_data in weather_slice:
+    condition_code =(hour_data["weather"][0]["id"])
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain: 
+    print("Your gona need to bring your umbrella")
+
+
+# print(weather_data["hourly"][0]["weather"][0]["id"])
