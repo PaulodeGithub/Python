d482baf9632bc45880eed3d3b5c0ae462132371f
PaulyGallagher <73261024+PaulyGallagher@users.noreply.github.com>
2023-06-22 16:03:57 +0100

    secured env secrets

--- Rainchecker/main.py
+++ Rainchecker/main.py
@@ -1,14 +1,18 @@
 import requests
+import os
 from twilio.rest import Client
 
+
+account_sid = os.environ.get("ACC_VAR")
+auth_token = os.environ.get("AUTH_TOKE")
 OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
-my_key = "INSERT YOUR API KEY HERE!"
+api_key = os.environ.get("WEATHER_API")
 
 
 weather_params = {
     "lat": 54.95,
     "lon" : -7.73333, 
-    "appid" : my_key,
+    "appid" :api_key,
     "exclude" : "comment,minutely,daily"
     }
 
@@ -25,7 +29,15 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain: 
-    print("Your gona need to bring your umbrella")
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+    from_='+15417222051',
+    body='You are a machine, better keep going',
+    to='+353873407070'
+    )
+
+    print(message.sid)
 
 
 # print(weather_data["hourly"][0]["weather"][0]["id"])
