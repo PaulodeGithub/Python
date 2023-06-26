846652ac1ba27b753512d9888d77e76474075b0f
Pauly Gallagher <paulgallagher1984@gmail.com>
2023-06-26 16:05:51 +0100

    creating graph

--- HabitTracker/main.py
+++ HabitTracker/main.py
@@ -1,5 +1,7 @@
 import requests
 
+TOKEN = "jfnrjgbgjsmdkigm"
+USERNAME = "pauly-gallagher",
 
 post_params = {
     "token" : "jfnrjgbgjsmdkigm",
@@ -9,6 +11,15 @@ post_params = {
 }
 
 Pixela_Endpoint = "https://pixe.la/v1/users"
+Graph_Endpooint = f"{Pixela_Endpoint}/{USERNAME}/graphs"
 
-response =requests.post(url=Pixela_Endpoint, json=post_params)
-print(response.text)
+
+
+# response =requests.post(url=Pixela_Endpoint, json=post_params)
+# print(response.text)
+
+graph_params = {
+    "token": "jfnrjgbgjsmdkigm", 
+}
+
+requests.post(url=Graph_Endpooint, json=graph_params)
