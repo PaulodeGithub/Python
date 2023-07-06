6ddce67a0fe72f98c33f8606a4ed70816734ebd2
Pauly Gallagher <paulygallagher1984@gmail.com>
2023-07-06 13:18:54 +0100

    selenium on windows challenges

--- Selenium/main.py
+++ Selenium/main.py
@@ -1,16 +1,28 @@
-
+from selenium.webdriver.common.by import By
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-chrome_driver_path ="C:\Development\chromedriver.exe"
+# Path to your ChromeDriver executable
+chrome_driver_path = "C:\Development\chromedriver.exe"
 
-driver = webdriver.Chrome()
+# Set the ChromeDriver path and version
+selenium_service = Service(chrome_driver_path, chrome_version=114)
 
-driver.get("https://www.amazon.com/ano-morte-Ayrton-Senna-Portuguese/dp/1548656461/ref=sr_1_4?keywords=books+ayrton+senna&qid=1688569006&refinements=p_n_feature_nine_browse-bin%3A3291445011&rnid=3291435011&s=books&sr=1-4")
+# Create ChromeOptions and specify headless mode if desired
+chrome_options = webdriver.ChromeOptions()
+chrome_options.headless = True
 
-driver.find_element(by="a-page")
+# Create the WebDriver instance
+driver = webdriver.Chrome(service=selenium_service, options=chrome_options)
 
+# Open the desired URL
+driver.get("https://en.wikipedia.org/wiki/Brazil")
 
+# Find the <title> element and print its text
+title_element = driver.find_element(By.XPATH, "//head/title")
+print(title_element.get_attribute("textContent"))
 
-
-# driver.close()
+# Quit the WebDriver
 driver.quit()
+
+
