07e1b9f53fe96f29b766324392233344f00568c3
Pauly Gallagher <paulygallagher1984@gmail.com>
2023-09-04 14:08:42 +0100

    added debuggermode

--- WebDevFlask/app.py
+++ WebDevFlask/app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/fearless")
+def be_fearless():
+    return "You a fearless animal"
+
+@app.route("/<name>")
+def username(name):
+    return f"You are unstoppable {name}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
